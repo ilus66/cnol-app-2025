9e0662d19eeae6c028d30531c598dea1037aa953
ilus66 <ilus1966@gmail.com>
2025-06-23 23:20:16 +0100

    Delete pages/test-vapid.js

--- pages/test-vapid.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export default function TestVapidKey() {
-  useEffect(() => {
-    console.log("VAPID public key:", process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY);
-  }, []);
-
-  return <div>Check the console for the VAPID public key.</div>;
-}
