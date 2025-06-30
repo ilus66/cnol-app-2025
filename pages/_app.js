752607c36ff45525d85a47e68ed7a559a218bfae
ilus66 <ilus1966@gmail.com>
2025-06-30 23:34:18 +0100

    fix: import CSS global de l'éditeur Markdown dans _app.js (Next.js build fix)

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ import { Toaster } from 'react-hot-toast';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import UpdateNotification from '../components/UpdateNotification'; // Importez le nouveau composant
+import '@uiw/react-md-editor/dist/mdeditor.css';
+import '@uiw/react-markdown-preview/dist/markdown.css';
 
 const theme = createTheme({
   palette: {
