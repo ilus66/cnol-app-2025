4ecd7f478a6f7f7043abee39925bf110c18bcaf0
cnol2025 <cnol.maroc@gmail.com>
2025-07-04 10:46:39 +0100

    Update send-whatsapp.js

--- pages/api/send-whatsapp.js
+++ pages/api/send-whatsapp.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Méthode non autorisée' });
 
   const { to, text } = req.body;
-  const apiKey = 'VOTRE_CLE_API'; // Remplace par ta clé API valide
+  const apiKey = '110f5e951d8effdf46eb4e3ddce932b5b3f48f4d31c9992e30d5c25e3ad4c030'; // Remplace par ta clé API valide
 
   try {
     const response = await fetch('https://wasenderapi.com/api/send-message', {
