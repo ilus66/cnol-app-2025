6005ec933255665f803dcf522a86813dfb90c4dc
ilus66 <ilus1966@gmail.com>
2025-07-19 12:01:55 +0100

    fix: décale le code-barres légèrement vers la droite pour alignement parfait avec le titre sur test-badge

--- pages/test-badge.js
+++ pages/test-badge.js
@@ -36,8 +36,8 @@ export default function TestBadge() {
           overflow: 'hidden',
         }}>
           {/* Code-barres en haut à gauche */}
-          <Box sx={{ width: '100%', display: 'flex', alignItems: 'flex-start', pt: 3, pb: 0, pl: 3 }}>
-            <Box sx={{ mr: 2, mt: 1 }}>
+          <Box sx={{ width: '100%', display: 'flex', alignItems: 'flex-start', pt: 3, pb: 0, pl: 5 }}>
+            <Box sx={{ mr: 2.5, mt: 1 }}>
               <Barcode value={badgeCode} width={2} height={24} displayValue={false} margin={0} style={{ width: 95 }} />
             </Box>
             <Box sx={{ flex: 1 }} />
